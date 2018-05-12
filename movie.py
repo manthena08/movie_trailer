0b125169ab5fefa742326700941dbb5400a07dc2
Varma Manthena <manthena08@users.noreply.github.com>
2018-05-12 16:13:54 -0600

    List for movie trailers

---
+++ movie.py
@@ -0,0 +1,7 @@
+class Movie:
+    def __init__(self,title, imageUrl,videoUrl):
+        self.title = title
+        self.trailer_youtube_url = videoUrl
+        self.poster_image_url = imageUrl
+
+    
