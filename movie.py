9a1a282678eb8341927ba151722e1a3c713f147b
Varma Manthena <manthena08@users.noreply.github.com>
2018-05-20 21:03:28 -0600

    Moving comments below

--- movie.py
+++ movie.py
@@ -1,12 +1,11 @@
-""" Movie is used to pass in the single movie information.
+class Movie:
+    """ Movie is used to pass in the single movie information.
     Args:
         title: The title of the movie.
         imageUrl: The image url of the movie.
         videoUrl: The video url of the movie.
-"""
+    """
 
-
-class Movie:
     def __init__(self, title, imageUrl, videoUrl):
         self.title = title
         self.trailer_youtube_url = videoUrl
